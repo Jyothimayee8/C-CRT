e18eb81115f9b9c951aacc18f67790bc4cb9b3b0
Jyothimayee8 <146567709+Jyothimayee8@users.noreply.github.com>
2025-02-21 09:44:35 +0530

    Update CONST2.c

--- CONST2.c
+++ CONST2.c
@@ -2,10 +2,11 @@
 #define LEN 5
 #define BRE 4
 int main(){
+	//#define len6 -->warning message
 	 int a,p;
 	a = LEN*BRE;
 	p = 2*(LEN+BRE);
 		printf("%d\n %d\n",a,p);
 	return 0;                                                                                                                                                                                                                               
 }
-//syntax = symbolicname constantvalue
+//syntax = #define symbolicname constantvalue
